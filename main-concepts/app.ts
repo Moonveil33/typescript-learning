36b1fa2e7e1e2f5c919d07f6a6111a5a424054ca
Alireza <andromedaa5692@gmail.com>
2026-09-20 23:11:54 +0330

    learning Dynamic & Static Types Languages

--- main-concepts/app.ts
+++ main-concepts/app.ts
@@ -1,9 +1,23 @@
-const number = 12
+// const number = 12
 // num1 = 10 // error for const 
 // TS good for Type Checking
 
-const sum = (num1:number, num2:number) => {
-   return (num1 + num2)
-}
+// const sum = (num1:number, num2:number) => {
+//    return (num1 + num2)
+// }
 
 // How TO Compile TS --> npx tsc app.ts
+// -------------------------------------
+// Static Type - Dynamic Type
+// Tooye JS mitooni variable ro reAsign koni ba data type haie mokhtalef
+
+
+// زبان های داینامیک تایپ : زبانی که متغیر ها تایپ ثابتی ندارند و ولیوهاشون هم همینطور .
+
+// زبان های استاتیک تایپ : زبان هایی که متغیرهاشون تایپ یکسان و مشخصی دارند و مدام در حال تغییر نیستند
+
+// let number = 90
+// number = 99
+// number = 'ali' // error type
+// -------------------------------------------
+
