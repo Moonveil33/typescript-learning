0a5d7894f7ebddd3d4b38639447e9b212376ee9e
Alireza <andromedaa5692@gmail.com>
2026-09-20 23:05:14 +0330

    How To compile TS

---
+++ main-concepts/app.js
@@ -0,0 +1,6 @@
+"use strict";
+const number = 12;
+// num1 = 10 // error for const 
+const sum = (num1, num2) => {
+    return (num1 + num2);
+};
---
+++ main-concepts/app.ts
@@ -0,0 +1,9 @@
+const number = 12
+// num1 = 10 // error for const 
+// TS good for Type Checking
+
+const sum = (num1:number, num2:number) => {
+   return (num1 + num2)
+}
+
+// How TO Compile TS --> npx tsc app.ts
